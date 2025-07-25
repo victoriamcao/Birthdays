0e34c461db535391b77825af4e58e93e6531fa73
Vicky Cao <miavickcao@gmail.com>
2025-07-25 10:31:34 -0700

    Added birthday and navigation stack

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [Friend(name: "Tenley", birthday: .now), Friend(name: "Joie", birthday: .now)]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
+        NavigationStack{
+            List(friends, id: \.name) { friend in
+                HStack{
+                    Text("\(friend.name)")
+                    Text("\(friend.birthday)")
+                }//HStack
+                .navigationTitle("Birthdays")
+            }//list
+        }//navstack
+    }//body
+}//struct
 #Preview {
     ContentView()
 }
---
+++ Birthdays/Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import Foundation
+
+class Friend {
+    var name : String
+    var birthday : Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
